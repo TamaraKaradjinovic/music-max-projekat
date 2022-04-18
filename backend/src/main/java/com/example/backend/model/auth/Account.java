3bddfc8ca2eece792ed5cc0bae01e2b35ef87556
TamaraKaradjinovic <tamarakaradjinovic@gmail.com>
2022-04-18 15:01:32 +0200

    "[fix] account class changed"

--- backend/src/main/java/com/example/backend/model/auth/Account.java
+++ backend/src/main/java/com/example/backend/model/auth/Account.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,9 +19,20 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String surname;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private Gender gender;
+
+    @Column(nullable = false)
+    private Date birthdate;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
---
+++ backend/src/main/java/com/example/backend/model/auth/Gender.java
@@ -0,0 +1,6 @@
+package com.example.backend.model.auth;
+
+public enum Gender {
+
+    MALE,FEMALE,OTHER
+}
