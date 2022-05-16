342c4facf67561a41f9e465abbccb22fd3bebc88
TamaraKaradjinovic <tamarakaradjinovic@gmail.com>
2022-05-16 23:04:45 +0200

    "[add] rate class added"

---
+++ backend/src/main/java/com/example/backend/model/music/Rate.java
@@ -0,0 +1,35 @@
+package com.example.backend.model.music;
+
+import com.example.backend.model.auth.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Rate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int rate;
+
+    @Column
+    private Date date;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Song song;
+
+}
